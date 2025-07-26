57750b34e18d4d9d4a9b7d533dd203346c3e3ae4
Thomas <thomasleblay98@gmail.com>
2025-07-26 17:33:42 +0200

    feat: expected teams performance: added columns.

--- socceranalytics/sql/performance/teams/fill_expected_performance_table.sql
+++ socceranalytics/sql/performance/teams/fill_expected_performance_table.sql
@@ -2,14 +2,19 @@ insert into analytics.staging_teams_expected_performance
 select
     '{season}' as season,
     coalesce(chp.name, c_cup.name) as comp,
-    ts.match, 
-    c.name as "Club", 
+    ts.match,
+    c.name as "Club",
     ts.played_home,
+    m.week,
+    m.round,
+    m.date,
     ts.xg::numeric, 
     ts.nb_shots_total::bigint as shots
 from season_{season}.team_stats ts 
-join season_{season}.match m on m.id = ts.match 
-join upper.club c on ts.team = m.competition || '_' || c.id
+join season_{season}.match m
+on m.id = ts.match
+join upper.club c
+on ts.team = m.competition || '_' || c.id
 left join upper.championship chp
 on m.competition = chp.id
 left join upper.continental_cup c_cup
