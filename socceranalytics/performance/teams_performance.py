b16642369b159da5017c03557fe1d78685b5bf31
Thomas <thomasleblay98@gmail.com>
2025-07-28 11:12:11 +0200

    fix: logging.

--- socceranalytics/performance/teams_performance.py
+++ socceranalytics/performance/teams_performance.py
@@ -53,6 +53,7 @@ class TeamsPerformance(BasePerformance, CompHelper):
 
                 if "UEFA" not in name_comp:
                     understat_comp = super().get_understat_comp_from_soccerstat(name_comp)
+                    log(f"{name_comp} - {understat_comp} - {season[:4]}", error=True)
                     xG_by_match = get_teams_xG(understat_comp, name_comp, season)
                     for match in xG_by_match:
                         self.db.df_from_query(
