97ebf51f24db4eaa72f6edf4ae06f620458528a2
Thomas <thomasleblay98@gmail.com>
2025-08-09 17:48:55 +0200

    feat: Team Player: update positions thanks to players performance table.

---
+++ socceranalytics/by_season_data/fill_position_team_player.py
@@ -0,0 +1,22 @@
+from socceranalytics.postgres.postgres_querying import PostgresQuerying
+from socceranalytics.utils.data_loader import DataLoader
+
+from socceranalytics.utils.logging import log
+
+
+class TeamPlayer:
+    def __init__(self, postgres_to_dataframe: PostgresQuerying):
+        self.db = postgres_to_dataframe
+        self.data_loader = DataLoader(postgres_to_dataframe)
+        self.sql_path = "socceranalytics.sql.by_season_data"
+
+    def update_team_player_table(self):
+        log("\tUpdating the 'Team Player' table of each season...")
+        by_season_query = self.db.read_sql_file(self.sql_path, "get_mos_played_positions.sql")
+
+        for season in self.data_loader.get_seasons():
+            self.db.execute_query(
+                by_season_query.format(
+                    season=season,
+                )
+            )
---
+++ socceranalytics/sql/by_season_data/get_most_played_positions.sql
@@ -0,0 +1,42 @@
+with players_performance as (
+    select
+        id_team,
+        id_player,
+        array_cat(home_positions, away_positions) as positions
+    from analytics.staging_players_performance
+    where season = '{{ season }}'
+),
+players_positions as (
+	select id_team, id_player, unnest(positions) as positions
+	from players_performance
+),
+positions_freq as (
+	select id_team, id_player, positions, count(*) as freq
+	from players_positions
+	group by id_team, id_player, positions
+	order by id_player, freq desc
+),
+most_played_positions as (
+	select id_team, id_player, positions
+	from (
+		select
+			id_team,
+			id_player,
+			positions,
+			freq,
+			ROW_NUMBER() OVER (PARTITION BY id_team, id_player ORDER BY freq DESC) AS rnk
+		from positions_freq
+	) ppe
+	where rnk <= 2
+)
+UPDATE season_{{ season }}.team_player tp
+SET tp.positions = p.positions
+FROM (
+    select
+        id_team,
+        id_player,
+        array_agg(distinct positions) as positions
+    from most_played_positions
+    group by id_team, id_player
+) p
+WHERE tp.team = p.id_team AND tp.player = p.id_player;
