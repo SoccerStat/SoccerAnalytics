e2bbbf6f71376a92e8a4b32a937ba4a97d1d6b57
thomaSLBY <89582199+thomaSLBY@users.noreply.github.com>
2025-09-13 00:13:28 +0200

    Feature/season threshold (#12)
    * fix: temporary excluding older seasons than 2018_2019.
    
    * fix: logging season.
    
    * fix: logging season. removed threshold.
    
    * Update version to 1.0.109-snapshot
    
    ---------
    
    Co-authored-by: github-actions[bot] <github-actions[bot]@users.noreply.github.com>

--- socceranalytics/sql/utils/schemas.sql
+++ socceranalytics/sql/utils/schemas.sql
@@ -8,7 +8,9 @@ begin
     return query 
 	SELECT schema_name::text as "Schema"
     FROM information_schema.schemata 
-    WHERE schema_name ~ 'season_\d';
+    WHERE schema_name ~ 'season_\d'
+    --and substr(schema_name::text, 8, 9) >= '2018_2019'
+    ;
 
 
 end;
