4ec277c9788f4d8c01be0574d459712dcce50a06
Thomas <thomasleblay98@gmail.com>
2025-11-02 18:24:36 +0100

    fix: re-put season filtering.

--- socceranalytics/sql/utils/schemas.sql
+++ socceranalytics/sql/utils/schemas.sql
@@ -9,11 +9,11 @@ begin
 	SELECT schema_name::text as "Schema"
     FROM information_schema.schemata 
     WHERE schema_name ~ 'season_\d'
-    -- AND case
-	-- 	when extract(month from current_date) > 7
-	-- 	then substr(schema_name::text, 8, 9) >= extract(year from current_date) || '_' || extract(year from current_date + interval '1 year')
-	-- 	else substr(schema_name::text, 8, 9) >= extract(year from current_date - interval '1 year') || '_' || extract(year from current_date)
-    -- end
+    AND case
+		when extract(month from current_date) > 7
+		then substr(schema_name::text, 8, 9) >= extract(year from current_date) || '_' || extract(year from current_date + interval '1 year')
+		else substr(schema_name::text, 8, 9) >= extract(year from current_date - interval '1 year') || '_' || extract(year from current_date)
+    end
     ;
 
 
