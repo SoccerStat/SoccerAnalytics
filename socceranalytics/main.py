cf30c6f64ccd47b2748d5fb7afa62cc37618d480
Thomas <thomasleblay98@gmail.com>
2026-01-18 11:12:13 +0100

    fix: get all teams.

--- socceranalytics/main.py
+++ socceranalytics/main.py
@@ -31,7 +31,7 @@ def run(args):
     teams_performance.process_mapping_clubs_table()
 
     all_teams = AllTeams(db)
-    all_teams.process_all_teams_table(min_season, max_season)
+    all_teams.process_all_teams_table()
 
     players_performance = PlayersPerformance(db)
     players_performance.process_performance_table(min_season, max_season)
