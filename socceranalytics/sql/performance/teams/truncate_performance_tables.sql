994adc126c0d8caab4475fc91e453ecc2d0730c8
Thomas <thomasleblay98@gmail.com>
2026-07-14 20:05:10 +0200

    fix: truncate understat table only for a given season.

--- socceranalytics/sql/performance/teams/truncate_performance_tables.sql
+++ socceranalytics/sql/performance/teams/truncate_performance_tables.sql
@@ -1,3 +1,3 @@
 DELETE FROM analytics.staging_teams_performance WHERE season = '{season}';
 --TRUNCATE TABLE analytics.staging_teams_expected_performance;
-TRUNCATE TABLE understat.staging_teams_understat_performance;
+TRUNCATE TABLE understat.staging_teams_understat_performance WHERE season = '{season}';
