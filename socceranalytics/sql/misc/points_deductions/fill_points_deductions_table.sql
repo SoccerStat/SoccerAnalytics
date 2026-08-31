2790e149e713b3926a9925acaafab399004644bc
Thomas <thomasleblay98@gmail.com>
2026-08-31 17:45:42 +0200

    fix: query

--- socceranalytics/sql/misc/points_deductions/fill_points_deductions_table.sql
+++ socceranalytics/sql/misc/points_deductions/fill_points_deductions_table.sql
@@ -1,19 +1,16 @@
-WITH points_deductions AS (
-    SELECT
-        '{season}' AS season,
-        competition AS id_comp,
-        chp.name AS competition,
-        t.id AS id_team,
-        c.name AS club,
-        points_deductions
-    FROM season_{season}.team t
-    LEFT JOIN upper.championship chp
-    on t.competition = chp.id
-    LEFT JOIN upper.continental_cup c_cup
-        on t.competition = c_cup.id
-    LEFT JOIN upper.club c
-        on t.club = c.id
-    WHERE points_deductions IS NOT NULL
-)
 INSERT INTO analytics.staging_teams_points_deductions
-SELECT * FROM points_deductions;
+SELECT
+    '{season}' AS season,
+    competition AS id_comp,
+    chp.name AS competition,
+    t.id AS id_team,
+    c.name AS club,
+    points_deductions
+FROM season_{season}.team t
+LEFT JOIN upper.championship chp
+on t.competition = chp.id
+LEFT JOIN upper.continental_cup c_cup
+    on t.competition = c_cup.id
+LEFT JOIN upper.club c
+    on t.club = c.id
+WHERE points_deductions IS NOT NULL;
